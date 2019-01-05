49f187212a7ab16cc53d65618e918251495d60ef
Gladis NDOUAB'S <gladis.douabalet@web.de>
2019-01-05 23:44:25 +0100

    launch RMI Registry before publish objects and at the end stop it

--- FilesReader/src/Source.java
+++ FilesReader/src/Source.java
@@ -3,7 +3,10 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 import de.ndsoft.filesReader.FileImpl;
 
@@ -13,8 +16,19 @@ public class Source {
 		// TODO Auto-generated method stub
 		final Remote obj = new FileImpl("c:\\myFile.txt");
 		final Remote remoteStub = UnicastRemoteObject.exportObject(obj, 19001);
-		Naming.rebind ("//localhost/FileServer", remoteStub);
-		Naming.unbind ("//localhost/FileServer");
+		// Starts RMI Registry into the program.
+		final Registry registry = LocateRegistry.createRegistry (18001);
+		Naming.rebind ("//localhost:18001/FileServer", remoteStub);
+		Naming.unbind ("//localhost:18001/FileServer");
+		
+		String resp = "";
+		while (resp != "quit") {
+			Scanner s = new Scanner(System.in);
+			resp = s.nextLine();
+		}
+		
+		// Stops RMI Registry.
+		UnicastRemoteObject.unexportObject (registry, true);
 	}
 
 }
