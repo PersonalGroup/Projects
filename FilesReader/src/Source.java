57350d10bbf33ea554dfd3f2fea8e67b6bd38904
Gladis NDOUAB'S <gladis.douabalet@web.de>
2019-01-05 23:22:13 +0100

    implementation

---
+++ FilesReader/src/Source.java
@@ -0,0 +1,18 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import de.ndsoft.filesReader.FileImpl;
+
+public class Source {
+
+	public static void main(String[] args) throws RemoteException, MalformedURLException {
+		// TODO Auto-generated method stub
+		final Remote obj = new FileImpl("c:\\myFile.txt");
+		final Remote remoteStub = UnicastRemoteObject.exportObject(obj);
+		Naming.rebind ("//localhost/FileServer", remoteStub);
+	}
+
+}
