c690ca093e949ea251a7dd8062032eddf1ffb3f5
Gladis NDOUAB'S <gladis.douabalet@web.de>
2019-01-05 23:27:48 +0100

    publish the link "FileServer" only one times

--- FilesReader/src/Source.java
+++ FilesReader/src/Source.java
@@ -1,5 +1,6 @@
 import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -8,11 +9,12 @@ import de.ndsoft.filesReader.FileImpl;
 
 public class Source {
 
-	public static void main(String[] args) throws RemoteException, MalformedURLException {
+	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
 		// TODO Auto-generated method stub
 		final Remote obj = new FileImpl("c:\\myFile.txt");
 		final Remote remoteStub = UnicastRemoteObject.exportObject(obj, 19001);
 		Naming.rebind ("//localhost/FileServer", remoteStub);
+		Naming.unbind ("//localhost/FileServer");
 	}
 
 }
