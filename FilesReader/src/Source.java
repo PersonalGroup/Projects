960b2c34cceab46668eef49e4128a08e3e4d0a5a
Gladis NDOUAB'S <gladis.douabalet@web.de>
2019-01-06 10:30:52 +0100

    Bug fixed

--- FilesReader/src/Source.java
+++ FilesReader/src/Source.java
@@ -14,21 +14,29 @@ public class Source {
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
 		// TODO Auto-generated method stub
+		// Open port 1099, 19001 and 18001 on the firewall.
 		final Remote obj = new FileImpl("c:\\myFile.txt");
 		final Remote remoteStub = UnicastRemoteObject.exportObject(obj, 19001);
 		// Starts RMI Registry into the program.
 		final Registry registry = LocateRegistry.createRegistry (18001);
 		Naming.rebind ("//localhost:18001/FileServer", remoteStub);
-		Naming.unbind ("//localhost:18001/FileServer");
 		
 		String resp = "";
-		while (resp != "quit") {
+		while (!resp.contentEquals("quit")) {
+			System.out.println("write \"quit\" to terminate the program: ");
 			Scanner s = new Scanner(System.in);
 			resp = s.nextLine();
+			System.out.println("you are writen: " + resp);
+			//s.close();
 		}
+		
+		Naming.unbind ("//localhost:18001/FileServer");
 		
 		// Stops RMI Registry.
 		UnicastRemoteObject.unexportObject (registry, true);
+		
+		System.out.println("Program end.");
+		System.exit(0);
 	}
 
 }
--- TestSample/src/Source.java
+++ TestSample/src/Source.java
@@ -9,7 +9,7 @@ public class Source {
 
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
 		// TODO Auto-generated method stub
-		final FileInterface obj = (FileInterface) Naming.lookup ("//localhost/FileServer");
-		final byte[] filedata = obj.downloadFile ("testFileDownloaded.txt");
+		final FileInterface obj = (FileInterface)Naming.lookup("//localhost:18001/FileServer");
+		final byte[] filedata = obj.downloadFile("C:\\Users\\Douabalet\\Documents\\Projects\\testFileDownloaded.txt");
 	}
 }
